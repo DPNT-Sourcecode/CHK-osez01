7802c1563813a9bb69f554b5de0d87225d861d41
sbx_user1060 <sbx_user1060@ip-10-128-184-170.eu-west-2.compute.internal>
2018-09-25 12:55:44 +0000

    Tue Sep 25 12:55:44 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,32 +2,59 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.Objects;
+
 public class CheckoutSolution {
+    private int anInt;
+
     public Integer checkout(String skus) {
         int aCount = 0;
+        int aDeal = 0;
+        int aTotal = 0;
+        
         int bCount = 0;
+        int bTotal = 0;
+        
         int cCount = 0;
+        int cTotal = 0;
+        
         int dCount = 0;
+        int dTotal = 0;
         // check for legal input
-        // parse string
+        if(skus.equals("")) {
+            return -1;
+        }
+        // parse string and count each SKU
         // format "AAABB"?
         System.out.print(skus);
         for(char SKU: skus.toCharArray()) {
-            if(SKU == 'A') {
-               aCount++;
+            switch (SKU) {
+                case 'A': 
+                    // Either there is a deal and restart counting
+                    if(aCount == 3){
+                        aCount = 0;
+                        aDeal++;
+                    } else {
+                        // Or count Item
+                        aCount++;
+                    }
+                    break;
+                case 'B': if(aCount == 3){
+                    anInt;
+                    aDeal++;
+                } else {
+                    // Or count Item
+                    aCount++;
+                }
+                break;
+                case 'C': cCount++;
+                case 'D': dCount++;
+                default: return -1;
             }
-            if(SKU == 'B') {
-                bCount++;
-            }
-            if(SKU == 'C') {
-                cCount++;
-            }
-            if(SKU == 'D') {
-                dCount++;
-            }
+
         }
-        System.out.print(aCount);
-        // count each item
+        // Total = item count + deal count
+        aTotal = (50 * aCount) + (aDeal * 130);
         // check for offers
         // sum up
 //        throw new SolutionNotImplementedException();
